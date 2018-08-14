d0a41cfab45356c648333f23431e66c5057d2e4e
UT302 <nikab.94@gmail.com>
2018-08-14 08:46:38 +0300

    initial commit

---
+++ isle.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+import math 
+
+
+
+def player():
+    hp = 100
+    st = 100
+    mp = 100
+    exp = 0
+    lvl = 1
+
+def sword_slash():
+    dmg = 15 
+    acr = 75
+    st_rq = 5 
+
+
+def little_cobold(): 
+    hp = 50
+    st = 25
+    exp = 25 
+
+while True: 
+
+
+    print("""You fighting with little cobold
+1. [Sword Slash]
+2. [Run]
+3. Oi mate, you reckon ye can help me? I have this problem with me mom
+"""
