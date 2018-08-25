a5f5b1a551eb0648a03db2df6dc497ff4f44ae2e
UT302 <nikab.94@gmail.com>
2018-08-25 17:58:44 +0300

    foundations

--- isle.py
+++ isle.py
@@ -1,31 +1,66 @@
-#!/bin/python3
-import math 
+from sys import exit
+import math
+from random import randint
+from textwrap import dedent
+from os import system
+
+class Scene(object):
+
+    def enter(self):
+        print("Not done yet. Subclass it and implement enter.()")
+        exit(1)
 
 
+class Engine(object):
 
-def player():
-    hp = 100
-    st = 100
-    mp = 100
-    exp = 0
-    lvl = 1
+    def __init__(self,scene_map):
+        self.scene_map = scene_map
 
-def sword_slash():
-    dmg = 15 
-    acr = 75
-    st_rq = 5 
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
 
-
-def little_cobold(): 
-    hp = 50
-    st = 25
-    exp = 25 
-
-while True: 
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
 
 
-    print("""You fighting with little cobold
-1. [Sword Slash]
-2. [Run]
-3. Oi mate, you reckon ye can help me? I have this problem with me mom
-"""
+        current_scene.enter()
+
+
+class Finished(Scene):
+
+    def enter(self):
+        print("Done")
+        exit(0)
+
+class Death(Scene):
+
+    def enter(self):
+        print("You are dead.")
+
+
+class Map(object):
+    scenes = {
+            'training_room':TrainingRoom(),
+            'death':Death()
+            'finished':Finished(),
+
+
+            }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+
+
+a_map = Map('training_room')
+a_game = Engine(a_map)
+a_game.play()
